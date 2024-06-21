00d81a752e179cc6a32b7530e474019c5ef33c29
manav <manavpatel13402003@gmail.com>
2024-06-21 18:26:49 +0530

    find friend program using list of map using  done

--- lib/lab_5/find_frined.dart
+++ lib/lab_5/find_frined.dart
@@ -1,12 +1,33 @@
 // WAP to create and read phonebook dictionary.
 
+import 'dart:io';
+
 void main() {
+  bool isCheck = true;
   List<Map<dynamic, dynamic>> phonebook = [
     {'name': 'manav', 'age': 12},
     {'name': 'jay', 'age': 12},
+    {'name': 'jay', 'age': 32},
     {'name': 'ajay', 'age': 12},
     {'name': 'vijay', 'age': 12},
   ];
 
-  for (int i = 0; i < phonebook.length; i++) {}
+  List<Map<dynamic, dynamic>> findFriends = [];
+
+  // user pass name and find in phonebook in and display data
+  stdout.write("Find Your Friend name : ");
+  String findName = stdin.readLineSync()!;
+
+  for (int i = 0; i < phonebook.length; i++) {
+    if (phonebook[i]['name'] == findName) {
+      isCheck = false;
+      findFriends.add(phonebook[i]);
+    }
+  }
+
+  if (isCheck) {
+    stdout.writeln("Data Not Found");
+  } else {
+    stdout.writeln(findFriends);
+  }
 }
